e863ceaa21d7d4e03799ed9a5f394de8dd41d0f6
artemij <artemijkolotov@gmail.com>
2026-01-21 18:11:23 +0300

    Добавить лог

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log('APP LISTENING ON PORT', port);
 }
 bootstrap();
